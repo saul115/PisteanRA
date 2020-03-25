5ee19511b2534318ccfe5ad15b2abcddfcd8cb71
Saul Augusto Gasca Farrera <sgfarrera@gmail.com>
2020-03-25 15:25:40 -0600

    Project Completed

--- GuitarraRSA/Assets/Scripts/ObjectRotation.cs
+++ GuitarraRSA/Assets/Scripts/ObjectRotation.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ObjectRotation : MonoBehaviour
 {
+    private Transform rotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotation = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotation.Rotate(Vector3.forward);
     }
 }

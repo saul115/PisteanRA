4f0bfed3b64a039846d43fce806e23bd9cc9a050
Saul Augusto Gasca Farrera <sgfarrera@gmail.com>
2020-03-25 14:05:15 -0600

    apk added

---
+++ GuitarraRSA/Assets/Scripts/CambiarEscena.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour
+{
+    public void changeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
